2a410513c5e27081249bde0ececd3e85bb43e02a
Umair Farooqi <100025597+umairfarooqi@users.noreply.github.com>
2025-01-15 19:57:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+class Student {
+    #id;
+    #name;
+    #email;
+    #course;
+
+    static studentList = [];
+
+    constructor(id, name, email, course) {
+        this.#id = id;
+        this.#name = name;
+        this.#email = email;
+        this.#course = course;
+    }
+
+    static addStudent(id, name, email, course) {
+        let student = new Student(id, name, email, course);
+        Student.studentList.push(student);
+        alert("Student added succefully");
+    }
+
+    static viewAllStudent() {
+        if (Student.studentList.length === 0) {
+            alert("No Student Found!")
+        } else {
+            for (let i = 0; i < Student.studentList.length; i++) {
+                let student = Student.studentList[i];
+                console.log("ID: " + student.#id);
+            console.log("Name: " + student.#name);
+            console.log("Email: " + student.#email);
+            console.log("Course: " + student.#course);
+            }
+        };
+    }
+
+    updateStudent(newId, newName, newEmail, newCourse){
+        this.#id = newId;
+        this.#name = newName;
+        this.#email = newEmail;
+        this.#course = newCourse;
+        alert("Student updated succefully");
+    }
+}
+Student.addStudent("01", "umair", "umair@gmail.com", "programing");
+Student.viewAllStudent();
+
+updateStudentRecord = new Student();
+updateStudentRecord.updateStudent("02", "UMAIR", "umair@jestravel.com", "Traveling");
+
+Student.viewAllStudent();
